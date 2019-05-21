9742da0ef72aac0b4e4edea2221b99ccc8202685
Demo <36319391+xjunming@users.noreply.github.com>
2019-05-21 15:21:27 +0800

    Update config.py

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir,'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'IMSECRET_KEY' or os.environ.get('SECRET_KEY')
-    RECAPTCHA_PUBLIC_KEY = 'IMRECAPTCHA_PUBLIC_KEY' or os.environ.get('RECAPTCHA_PUBLIC_KEY')
-    RECAPTCHA_PRIVATE_KEY = 'IMRECAPTCHA_PRIVATE_KEY' or os.environ.get('RECAPTCHA_PUBLIC_KEY')
+    RECAPTCHA_PUBLIC_KEY = 'PUBLIC_KEY' or os.environ.get('RECAPTCHA_PUBLIC_KEY')
+    RECAPTCHA_PRIVATE_KEY = 'PRIVATE_KEY' or os.environ.get('RECAPTCHA_PUBLIC_KEY')
